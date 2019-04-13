0bedf1a68a0658769e2c12842ff5f1ec0373c055
saraheyler <42948575+saraheyler@users.noreply.github.com>
2019-04-13 15:25:18 -0400

    Update block_verifier.rb

--- block_verifier.rb
+++ block_verifier.rb
@@ -1,10 +1,17 @@
-
 class Block_Verifier
-	class Block
+  class Block
+    attr_accessor :number
+    attr_accessor :prev_block_hash
+    attr_accessor :raw_transactions
+    attr_accessor :transactions
+    attr_accessor :timestamp
+    attr_accessor :block_hash
 
 		def initialize
 			number = 0
 			prev_block_hash = 0
+      raw_transactions = 0
+      transactions = []
 			timestamp = 0
 			block_hash = 0
 		end
@@ -12,7 +19,22 @@ class Block_Verifier
 	end
 
 		# TEST #
-		def check_block(block)
-			puts block.to_s
+		def check_block(blocks)
+			blocks.each do |b|
+        block = Block.new
+        attributes = b.split('|')
+        block.number = attributes[0]
+        block.prev_block_hash = attributes[1]
+        block.raw_transactions = attributes[2]
+        block.transactions = block.raw_transactions.split(':')
+        block.timestamp = attributes[3]
+        block.block_hash = attributes[4]
+        puts block.number
+        puts block.prev_block_hash
+        puts block.raw_transactions
+        puts block.transactions
+        puts block.timestamp
+        puts block. block_hash
+      end
 		end
 end
