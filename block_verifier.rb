5d7e49bfa508655ac7ffdcf209adb88e88aa5e1f
Brandon La <bnl22@pitt.edu>
2019-04-13 14:56:07 -0400

    Add files via upload
    block class

--- block_verifier.rb
+++ block_verifier.rb
@@ -1,8 +1,18 @@
 
 class Block_Verifier
+	class Block
 
-	def check_block(block)
-		puts block.to_s
+		def initialize
+			number = 0
+			prev_block_hash = 0
+			timestamp = 0
+			block_hash = 0
+		end
+
 	end
 
+		# TEST #
+		def check_block(block)
+			puts block.to_s
+		end
 end
