a6d5c4062beeb48d8d67e517e6d3b6ec4ccfd696
saraheyler <42948575+saraheyler@users.noreply.github.com>
2019-04-13 15:25:50 -0400

    Update verifier.rb

--- verifier.rb
+++ verifier.rb
@@ -19,9 +19,9 @@ valid_args = args.arg_check ARGV
 # If valid, begin Rubyist's Ruby Hunt simulator. Else, display usage syntax and exit
 if valid_args
   @verify_block = Block_Verifier.new
-  File.open(ARGV[0]).each do |line|
-    @verify_block.check_block(line)
-  end
+  file = File.open(ARGV[0])
+  blocks = file.read.split("\n")
+  @verify_block.check_block(blocks)
 else
   arg_error
 end
